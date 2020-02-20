5d124bca461ecd6d04f2b76dddbea8d3a330d6e4
John McDowall <john@mcdowall.info>
2020-02-19 18:22:10 -0800

    DOOM config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -11,6 +11,17 @@
 (menu-bar-mode -1) 
 (tool-bar-mode -1)
 (toggle-scroll-bar -1)
+;;
+;; platform
+;;
+
+;; Define some platform-specific variables.
+(setq is-terminal (equal window-system nil))
+(setq is-gui (equal window-system 'ns))
+(setq is-xserve (equal window-system 'x))
+(setq is-osx (equal system-type 'darwin))
+(setq is-wsl (string-match "[Mm]icrosoft" operating-system-release))
+(setq is-gnu (memq system-type '(gnu gnu/linux gnu/kfreebsd)))
 
 
 
@@ -28,7 +39,7 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "monospace" :size 32))
+(setq doom-font (font-spec :family "Delugia Nerd Font" :size 16))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -59,24 +70,91 @@
 ;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
 ;; they are implemented.
 
-(defun wsl-copy (start end)
+;;;
+;; copy / paste
+;; JMD - Lifted from https://github.com/wusticality/emacs/blob/master/.emacs.d/init.el
+;;
+
+;; Copy / paste on mac.
+(when (and is-osx is-terminal)
+  ;; Copy from the clipboard.
+  (defun mac-copy ()
+    (shell-command-to-string "pbpaste"))
+
+  ;; Paste from the clipboard.
+  (defun mac-paste (text &optional push)
+    (let ((process-connection-type nil))
+      (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+        (process-send-string proc text)
+        (process-send-eof proc))))
+
+  ;; Install the commands.
+  (setq interprogram-paste-function 'mac-copy)
+  (setq interprogram-cut-function 'mac-paste))
+
+;; Copy / paste on wsl.
+;;
+;; For this I had help from the following sources:
+;;
+;; https://github.com/redguardtoo/emacs.d
+;; http://blog.binchen.org/posts/copypaste-in-emacs.html
+;;
+;; For things to work on wsl, you need to have "clip.exe"
+;; and Powershell ("powershell.exe") in your path. For Powershell,
+;; you also need to have the following extension installed:
+;;
+;; https://github.com/mklement0/ClipboardText
+
+(when (or is-wsl is-terminal is-xserve)
+  (defun my-clipboard-set (value)
+    "Sets the clipboard value."
+    (let ((program (executable-find "clip.exe")))
+      (with-temp-buffer
+        (insert value)
+        (cond
+         (program
+          (call-process-region (point-min) (point-max) program))
+         (t
+          (call-process-region (point-min) (point-max) "xsel" nil nil nil "--clipboard" "--input"))))))
+
+  (defun my-clipboard-get ()
+    "Gets the clipboard value."
+    (let ((program (executable-find "powershell.exe")))
+      (cond
+       (program
+        (substring
+         (with-output-to-string
+           (with-current-buffer standard-output
+             (let ((coding-system-for-read 'dos))
+               (call-process program nil t nil "-command" "Get-Clipboard"))))
+         0 -1))
+       (t
+        (with-output-to-string
+          (with-current-buffer standard-output
+            (call-process "xsel" nil t nil "--clipboard" "--output")))))))
+
+  (defun my-clipboard-copy (start end)
+    "Copy to the clipboard."
     (interactive "r")
-      (shell-command-on-region start end "clip.exe"))
+    (my-clipboard-set (buffer-substring-no-properties start end)))
 
-(global-set-key
-   (kbd "C-c C-c")
-    'wsl-copy)
-
-(defun wsl-paste ()
+  (defun my-clipboard-paste ()
+    "Paste from the clipboard."
     (interactive)
-      (let ((wslbuffername "wsl-temp-buffer"))
-      (get-buffer-create wslbuffername)
-      (with-current-buffer wslbuffername
-        (insert (let ((coding-system-for-read 'dos))
-        (shell-command "powershell.exe -command 'Get-Clipboard' 2> /dev/null" wslbuffername nil))))
-      (insert-buffer wslbuffername)
-      (kill-buffer wslbuffername)))
+    (insert (my-clipboard-get)))
 
-(global-set-key
-   (kbd "C-c C-v")
-    'wsl-paste)
+  ;; Keybindings for clipboard copy / paste.
+  (global-set-key (kbd "C-c C-c") 'my-clipboard-copy)
+  (global-set-key (kbd "C-c C-v") 'my-clipboard-paste))
+
+;; Set initial frame size and position
+(defun my/set-initial-frame ()
+  (let* ((base-factor 0.70)
+	(a-width (* (display-pixel-width) base-factor))
+        (a-height (* (display-pixel-height) base-factor))
+        (a-left (truncate (/ (- (display-pixel-width) a-width) 2)))
+	(a-top (truncate (/ (- (display-pixel-height) a-height) 2))))
+    (set-frame-position (selected-frame) a-left a-top)
+    (set-frame-size (selected-frame) (truncate a-width)  (truncate a-height) t)))
+(setq frame-resize-pixelwise t)
+(my/set-initial-frame)
