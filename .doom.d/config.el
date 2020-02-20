304840abae0cc46891316b84acd791c0b9851874
John McDowall <john@mcdowall.info>
2020-02-19 22:36:53 -0800

    Rollup

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -8,8 +8,8 @@
 (add-to-list 'default-frame-alist
                           '(ns-appearance . dark))
 
-(menu-bar-mode -1) 
-(tool-bar-mode -1)
+;(menu-bar-mode -1)
+;(tool-bar-mode -1)
 (toggle-scroll-bar -1)
 ;;
 ;; platform
