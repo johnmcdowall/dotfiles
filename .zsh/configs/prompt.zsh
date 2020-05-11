8259e121716ed7781c4a741165c67e2839935483
John McDowall <john@mcdowall.info>
2020-05-11 03:28:47 -0700

    Chevron prompt

--- .zsh/configs/prompt.zsh
+++ .zsh/configs/prompt.zsh
@@ -10,5 +10,5 @@ setopt promptsubst
 
 # Allow exported PS1 variable to override default prompt.
 if ! env | grep -q '^PS1='; then
-  PS1='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}%{$fg_bold[blue]%}%c%{$reset_color%}$(git_prompt_info) %# '
+  PS1='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}%{$fg_bold[blue]%}%c%{$reset_color%}$(git_prompt_info) $fg_bold[green] '
 fi
